f1295f12c9876d92e294f9f26eec399fa72ae90a
Janelle Cai <jcai18@mit.edu>
2024-04-17 17:11:55 -0700

    contours and more bbox things

--- all_seaing_ros2/all_seaing_vehicle/perception/color_segmentation.py
+++ all_seaing_ros2/all_seaing_vehicle/perception/color_segmentation.py
@@ -18,6 +18,9 @@ class ColorSegmentation(Node):
         self.bbox_pub = self.create_publisher(
             LabeledBoundingBox2DArray, "/perception_suite/bounding_boxes", qos_profile
         )
+        self.img_pub = self.create_publisher(
+            Image, "/perception_suite/segmented_image", qos_profile
+        )
         self.img_sub = self.create_subscription(
             Image,
             "/perception_suite/image",  # Remap this to correct topic
@@ -52,40 +55,52 @@ class ColorSegmentation(Node):
         # change later !!!
         threshold_pixels = 1000
 
+
+        bboxes = LabeledBoundingBox2DArray()
+        bboxes.header.stamp = self.get_clock().now().to_msg()
         for color in colors:
             if color == "red2":
                 continue
             h_min, h_max, s_min, s_max, v_min, v_max = colors[color]
-            lower_limit = np.array([h_min, s_min, v_min])
-            upper_limit = np.array([h_max, s_max, v_max])
+            lower_limit = [h_min, s_min, v_min]
+            upper_limit = [h_max, s_max, v_max]
             mask = cv2.inRange(hsv_img, lower_limit, upper_limit)
             if color == "red":
                 h_min, h_max, s_min, s_max, v_min, v_max = colors["red2"]
-                lower_limit = np.array([h_min, s_min, v_min])
-                upper_limit = np.array([h_max, s_max, v_max])
+                lower_limit = [h_min, s_min, v_min]
+                upper_limit = [h_max, s_max, v_max]
                 mask = mask + cv2.inRange(hsv_img, lower_limit, upper_limit)
-            num_valid = cv2.countNonZero(mask)
-            if num_valid > threshold_pixels:
-                result = cv2.bitwise_and(img, img, mask=mask)
-                result_dict[color] = result
 
-            bboxes = LabeledBoundingBox2DArray()
-            bboxes.header.stamp = self.get_clock().now().to_msg()
-            for color in result_dict:
-                new_image = result_dict[color]
+            # findContours on mask
+            # imgray = cv2.cvtColor(mask, cv.COLOR_BGR2GRAY)
+            # ret, thresh = cv2.threshold(mask, 127, 255, 0)
+            contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            for contour in contours:
+                x, y, w, h = cv2.boundingRect(contour)
                 bbox = LabeledBoundingBox2D()
-                # bbox.min_x = int(row["xmin"])
-                # bbox.min_y = int(row["ymin"])
+                bbox.min_x = x
+                bbox.min_y = y
+                bbox.max_x = x + w
+                bbox.max_y = y + h
+                bbox.label = color
 
-                # bbox.max_x = int(row["xmax"])
-                # bbox.max_y = int(row["ymax"])
-
-                # bbox.label = row["class"]
-                # bboxes.boxes.append(bbox)
-                # cv2.rectangle(
-                #     img, (bbox.min_x, bbox.min_y), (bbox.max_x, bbox.max_y), (255, 0, 0), 4
-                # )
+                bboxes.boxes.append(bbox)
                 
+                # draw a rectangle on the image that is the bounding box
+                cv2.rectangle(
+                    img, (bbox.min_x, bbox.min_y), (bbox.max_x, bbox.max_y), (255, 0, 0), 4
+                )
+
+            self.img_pub.publish(self.bridge.cv2_to_imgmsg(img, "rgb8"))
+            self.bbox_pub.publish(bboxes)
+
+
+            # num_valid = cv2.countNonZero(mask)
+            
+            # if num_valid > threshold_pixels:
+            #     result = cv2.bitwise_and(img, img, mask=mask)
+            #     result_dict[color] = result
+
                     
 
 
