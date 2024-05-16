fc5e2b3cae6fb3561be022ae9d1a0ec1510fc3b3
Toya Takahashi <toyatakahashi522@gmail.com>
2024-05-16 14:13:32 -0400

    improved sensor bringup launch file

--- all_seaing_ros2/launch/sensor_bringup.launch.py
+++ all_seaing_ros2/launch/sensor_bringup.launch.py
@@ -9,69 +9,17 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
-    # velodyne pointcloud convert node
-    tf_params = {}
-    tf_params["calibration"] = os.path.join(
-        get_package_share_directory(
-            "velodyne_pointcloud"
-        ), 
-        "params",
-        "32db.yaml"
-    )
-    tf_params["min_range"] = 0.9
-    tf_params["max_range"] = 130.0
-    tf_params["view_direction"] = 0.0
-    tf_params["organize_cloud"] = True
-    transform_container = ComposableNodeContainer(
-        name="velodyne_pointcloud_transform_container",
-        namespace="",
-        package="rclcpp_components",
-        executable="component_container",
-        composable_node_descriptions=[
-            ComposableNode(
-                package="velodyne_pointcloud",
-                plugin="velodyne_pointcloud::Convert",
-                name="velodyne_convert_node",
-                parameters=[tf_params],
+    # Velodyne LiDAR
+    lidar_ld = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            get_package_share_directory(
+                "all_seaing_vehicle"
             ),
-        ],
-        output="both",
+            "/launch/32e_points.launch.py"
+        ])
     )
 
-    # velodyne laserscan
-    laserscan_node = Node(
-        package="velodyne_laserscan",
-        executable="velodyne_laserscan_node",
-        output="screen",
-    )
-
-    # velodyne driver
-    driver_params = {}
-    driver_params["device_ip"] = "192.168.1.201"
-    driver_params["read_once"] = False
-    driver_params["read_fast"] = False
-    driver_params["repeat_delay"] = 0.0
-    driver_params["frame_id"] = "velodyne"
-    driver_params["model"] = "32E"
-    driver_params["rpm"] = 600.0
-    driver_params["port"] = 2368
-    driver_container = ComposableNodeContainer(
-        name="velodyne_driver_container",
-        namespace="",
-        package="rclcpp_components",
-        executable="component_container",
-        composable_node_descriptions=[
-            ComposableNode(
-                package="velodyne_driver",
-                plugin="velodyne_driver::VelodyneDriver",
-                name="velodyne_driver_node",
-                parameters=[driver_params],
-            ),
-        ],
-        output="both",
-    )
-
-    # mavros
+    # MAVROS
     mavros_ld = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             get_package_share_directory(
@@ -81,7 +29,7 @@ def generate_launch_description():
         ])
     )
 
-    # zed
+    # ZED camera
     zed_ld = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             get_package_share_directory(
@@ -91,22 +39,8 @@ def generate_launch_description():
         ])
     )
 
-    # pwm sender
-    pwm_sender_node = Node(
-        package="all_seaing_vehicle"
-    )
-
-    laserscan_node = Node(
-        package="velodyne_laserscan",
-        executable="velodyne_laserscan_node",
-        output="screen",
-    )
-
-
     return LaunchDescription([
-        driver_container, 
-        transform_container,
-        laserscan_node,
+        lidar_ld,
         mavros_ld,
         zed_ld
     ])
--- all_seaing_ros2/launch/xdrive_full_test_sim.launch.py
+++ all_seaing_ros2/launch/xdrive_full_test_sim.launch.py
@@ -26,62 +26,82 @@ def generate_launch_description():
             EmitEvent(event=Shutdown(reason="task manager lost heartbeat"))
         ]
     )
-    return LaunchDescription([
-        DeclareLaunchArgument("run_tasks", default_value=TextSubstitution(text="True")),
-        # state reporter
-        launch_ros.actions.Node(
-	        package="all_seaing_vehicle",
-            executable="nav_state_reporter",
-            remappings=[
-                ("/imu/data", "/wamv/sensors/imu/imu/data"),
-                ("/gps/fix", "/wamv/sensors/gps/gps/fix")
-            ]
-        ),
-        launch_ros.actions.Node(
-            package="robot_localization",
-            executable="ekf_node",
-            name="ekf_filter_node",
-            parameters=[robot_localization_params]),
-        launch_ros.actions.Node(
-            package="robot_localization",
-            executable="navsat_transform_node",
-            name="navsat_transform_node",
-            remappings=[("/gps/fix", "/wamv/sensors/gps/gps/fix")],
-            parameters=[robot_localization_params]),
-        launch_ros.actions.Node(
-            package="protobuf_client",
-            executable="protobuf_client_node",
-            output="screen"),
-        launch_ros.actions.Node(
-            package="all_seaing_vehicle",
-            executable="xdrive_controller.py",
-            name="controller",
-            parameters=[{"in_sim": True}]
-        ),
-        # task_manager_mode,
-        launch_ros.actions.Node(
-            package="all_seaing_vehicle",
-            executable="rviz_testing_helper.py",
-            name="rviz_testing_helper",
-            parameters=[{"bag_path": bag_path}]
-        ),
-        launch_ros.actions.Node(
-            package="keyboard",
-            executable="keyboard",
-            name="keyboard"
-        ),
-        launch_ros.actions.Node(
-            package="keyboard",
-            executable="keyboard_to_joy.py",
-            name="keyboard_to_joy",
-            parameters=[{"config_file_name": os.path.join(all_seaing_prefix, "params", "keyboard_config.yaml")}]
-        ),
-        launch_ros.actions.Node(
-            package="rviz2",
-            executable="rviz2",
-            arguments=["-d", os.path.join(all_seaing_prefix, "params", "rviz_config.rviz")],
-        ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([vrx_gz_prefix, "/launch/competition.launch.py"]),
-            launch_arguments = {"world": "sydney_regatta", "urdf": f"{all_seaing_prefix}/urdf/xdrive_wamv/wamv_target.urdf"}.items()),
-    ])
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "run_tasks", default_value=TextSubstitution(text="True")
+            ),
+            # state reporter
+            launch_ros.actions.Node(
+                package="all_seaing_vehicle",
+                executable="nav_state_reporter",
+                remappings=[
+                    ("/imu/data", "/wamv/sensors/imu/imu/data"),
+                    ("/gps/fix", "/wamv/sensors/gps/gps/fix"),
+                ],
+            ),
+            launch_ros.actions.Node(
+                package="robot_localization",
+                executable="ekf_node",
+                name="ekf_filter_node",
+                parameters=[robot_localization_params],
+            ),
+            launch_ros.actions.Node(
+                package="robot_localization",
+                executable="navsat_transform_node",
+                name="navsat_transform_node",
+                remappings=[("/gps/fix", "/wamv/sensors/gps/gps/fix")],
+                parameters=[robot_localization_params],
+            ),
+            launch_ros.actions.Node(
+                package="protobuf_client",
+                executable="protobuf_client_node",
+                output="screen",
+            ),
+            launch_ros.actions.Node(
+                package="all_seaing_vehicle",
+                executable="xdrive_controller.py",
+                name="controller",
+                parameters=[{"in_sim": True}],
+            ),
+            # task_manager_mode,
+            launch_ros.actions.Node(
+                package="all_seaing_vehicle",
+                executable="rviz_testing_helper.py",
+                name="rviz_testing_helper",
+                parameters=[{"bag_path": bag_path}],
+            ),
+            launch_ros.actions.Node(
+                package="keyboard", executable="keyboard", name="keyboard"
+            ),
+            launch_ros.actions.Node(
+                package="keyboard",
+                executable="keyboard_to_joy.py",
+                name="keyboard_to_joy",
+                parameters=[
+                    {
+                        "config_file_name": os.path.join(
+                            all_seaing_prefix, "params", "keyboard_config.yaml"
+                        )
+                    }
+                ],
+            ),
+            launch_ros.actions.Node(
+                package="rviz2",
+                executable="rviz2",
+                arguments=[
+                    "-d",
+                    os.path.join(all_seaing_prefix, "params", "rviz_config.rviz"),
+                ],
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [vrx_gz_prefix, "/launch/competition.launch.py"]
+                ),
+                launch_arguments={
+                    "world": "sydney_regatta",
+                    "urdf": f"{all_seaing_prefix}/urdf/xdrive_wamv/wamv_target.urdf",
+                }.items(),
+            ),
+        ]
+    )
