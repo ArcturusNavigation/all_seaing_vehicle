88b4b5bc5dd0c5667fcf2e76624de5ee71811545
Toya Takahashi <toyatakahashi522@gmail.com>
2024-09-09 14:41:31 -0400

    added waypoint marker to dashboard

--- all_seaing_controller/all_seaing_controller/controller_server.py
+++ all_seaing_controller/all_seaing_controller/controller_server.py
@@ -11,6 +11,7 @@ from all_seaing_interfaces.action import Waypoint
 from all_seaing_interfaces.msg import ControlMessage
 from nav_msgs.msg import Odometry
 from tf_transformations import euler_from_quaternion
+from visualization_msgs.msg import Marker
 
 
 class ControllerServer(Node):
@@ -19,13 +20,24 @@ class ControllerServer(Node):
 
         self.group = MutuallyExclusiveCallbackGroup()
 
+        self.timer_period = (
+            self.declare_parameter("timer_period", 1 / 60)
+            .get_parameter_value()
+            .double_value
+        )
+        self.global_frame_id = (
+            self.declare_parameter("global_frame_id", "odom")
+            .get_parameter_value()
+            .string_value
+        )
+
         self.waypoint_server = ActionServer(
             self,
             Waypoint,
             "waypoint",
             callback_group=self.group,
             execute_callback=self.waypoint_callback,
-            cancel_callback=self.waypoint_cancel_callback,
+            cancel_callback=self.cancel_callback,
         )
         self.odom_sub = self.create_subscription(
             Odometry,
@@ -35,10 +47,13 @@ class ControllerServer(Node):
             callback_group=self.group,
         )
         self.control_pub = self.create_publisher(ControlMessage, "control_options", 10)
+        self.marker_pub = self.create_publisher(Marker, "control_marker", 10)
 
         self.nav_x = 0.0
         self.nav_y = 0.0
         self.heading = 0.0
+        self.proc_count = 0
+        self.marker_ns = "control"
 
     def odom_callback(self, msg: Odometry):
         self.nav_x = msg.pose.pose.position.x
@@ -53,33 +68,72 @@ class ControllerServer(Node):
         )
 
     def send_waypoint(self, x, y, angular):
-        control_message = ControlMessage()
-        control_message.priority = 1
-        control_message.linear_control_mode = ControlMessage.WORLD_POSITION
-        control_message.angular_control_mode = ControlMessage.WORLD_POSITION
-        control_message.x = x
-        control_message.y = y
-        control_message.angular = angular
-        self.control_pub.publish(control_message)
+        control_msg = ControlMessage()
+        control_msg.priority = 1
+        control_msg.linear_control_mode = ControlMessage.WORLD_POSITION
+        control_msg.angular_control_mode = ControlMessage.WORLD_POSITION
+        control_msg.x = x
+        control_msg.y = y
+        control_msg.angular = angular
+        self.control_pub.publish(control_msg)
 
-    def waypoint_cancel_callback(self, cancel_request):
-        self.get_logger().info("Waypoint following canceled!")
+    def start_process(self, msg=None):
+        self.proc_count += 1
+        while self.proc_count >= 2:
+            time.sleep(self.timer_period)
+        if msg is not None:
+            time.sleep(self.timer_period)
+            self.get_logger().info(msg)
+
+    def end_process(self, msg=None):
+        self.delete_all_marker()
+        self.proc_count -= 1
+        if msg is not None:
+            self.get_logger().info(msg)
+
+    def delete_all_marker(self):
+        marker_msg = Marker()
+        marker_msg.header.frame_id = self.global_frame_id
+        marker_msg.header.stamp = self.get_clock().now().to_msg()
+        marker_msg.ns = self.marker_ns
+        marker_msg.action = Marker.DELETEALL
+        self.marker_pub.publish(marker_msg)
+
+    def visualize_waypoint(self, x, y):
+        marker_msg = Marker()
+        marker_msg.header.frame_id = self.global_frame_id
+        marker_msg.header.stamp = self.get_clock().now().to_msg()
+        marker_msg.ns = self.marker_ns
+        marker_msg.type = Marker.CYLINDER
+        marker_msg.action = Marker.ADD
+        marker_msg.pose.position.x = x
+        marker_msg.pose.position.y = y
+        marker_msg.pose.position.z = 2.0
+        marker_msg.scale.x = 0.4
+        marker_msg.scale.y = 0.4
+        marker_msg.scale.z = 8.0
+        marker_msg.color.a = 1.0
+        marker_msg.color.r = 1.0
+        marker_msg.color.g = 1.0
+        marker_msg.color.b = 1.0
+        self.marker_pub.publish(marker_msg)
+
+    def cancel_callback(self, cancel_request):
         return CancelResponse.ACCEPT
 
     def waypoint_callback(self, goal_handle):
+        self.start_process("Waypoint following started!")
+
         xy_threshold = goal_handle.request.xy_threshold
         theta_threshold = goal_handle.request.theta_threshold
         goal_x = goal_handle.request.x
         goal_y = goal_handle.request.y
-        goal_theta = goal_handle.request.theta
-        ignore_theta = goal_handle.request.ignore_theta
-
-        if ignore_theta:
-            goal_theta = math.atan2(goal_y - self.nav_y, goal_x - self.nav_x)
-
-        self.get_logger().info(
-            f"Navigating to x: {goal_x:.4}, y: {goal_y:.4}, theta: {goal_theta:.4}"
+        goal_theta = (
+            math.atan2(goal_y - self.nav_y, goal_x - self.nav_x)
+            if goal_handle.request.ignore_theta
+            else goal_handle.request.theta
         )
+        self.visualize_waypoint(goal_x, goal_y)
 
         feedback_msg = Waypoint.Feedback()
         while (
@@ -87,7 +141,13 @@ class ControllerServer(Node):
             or abs(goal_y - self.nav_y) > xy_threshold
             or abs(goal_theta - self.heading) > math.radians(theta_threshold)
         ):
+            if self.proc_count >= 2:
+                self.end_process("Waypoint following aborted!")
+                goal_handle.abort()
+                return Waypoint.Result()
+
             if goal_handle.is_cancel_requested:
+                self.end_process("Waypoint following canceled!")
                 goal_handle.canceled()
                 return Waypoint.Result()
 
@@ -96,18 +156,17 @@ class ControllerServer(Node):
             feedback_msg.y = self.nav_y
             feedback_msg.theta = self.heading
             goal_handle.publish_feedback(feedback_msg)
-            time.sleep(1 / 60)
+            time.sleep(self.timer_period)
 
+        self.end_process("Waypoint following completed!")
         goal_handle.succeed()
-        result = Waypoint.Result()
-        result.is_finished = True
-        return result
+        return Waypoint.Result(is_finished=True)
 
 
 def main(args=None):
     rclpy.init(args=args)
     node = ControllerServer()
-    executor = MultiThreadedExecutor()
+    executor = MultiThreadedExecutor(num_threads=2)
     executor.add_node(node)
     executor.spin()
     node.destroy_node()
--- all_seaing_navigation/all_seaing_navigation/waypoint_sender.py
+++ all_seaing_navigation/all_seaing_navigation/waypoint_sender.py
@@ -4,7 +4,6 @@ import rclpy
 from rclpy.action import ActionClient
 from rclpy.node import Node
 
-from action_msgs.msg import GoalStatus
 from all_seaing_interfaces.action import Waypoint
 from geometry_msgs.msg import PointStamped
 
@@ -29,12 +28,7 @@ class WaypointSender(Node):
             PointStamped, "/clicked_point", self.point_callback, 10
         )
 
-        self.goal_handle = None
-
     def point_callback(self, msg: PointStamped):
-        if self.goal_handle is not None and self.goal_handle.status == GoalStatus.STATUS_EXECUTING:
-            self.cancel_goal_future = self.goal_handle.cancel_goal_async()
-
         goal_msg = Waypoint.Goal()
         goal_msg.xy_threshold = self.xy_threshold
         goal_msg.theta_threshold = self.theta_threshold
@@ -44,16 +38,6 @@ class WaypointSender(Node):
 
         self.action_client.wait_for_server()
         self.send_goal_future = self.action_client.send_goal_async(goal_msg)
-        self.send_goal_future.add_done_callback(self.goal_response_callback)
-
-    def goal_response_callback(self, future):
-        self.goal_handle = future.result()
-        self.get_result_future = self.goal_handle.get_result_async()
-        self.get_result_future.add_done_callback(self.get_result_callback)
-
-    def get_result_callback(self, future):
-        result = future.result().result
-        self.get_logger().info(f"Waypoint following finished?: {result.is_finished}")
 
 
 def main(args=None):
