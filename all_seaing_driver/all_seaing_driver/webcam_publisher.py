5bd1bb3048e351024032feea28fc257f85e38b34
Linda Xue <lexue28@gmail.com>
2024-09-24 21:11:58 -0400

    added spin to webcam

--- all_seaing_driver/all_seaing_driver/webcam_publisher.py
+++ all_seaing_driver/all_seaing_driver/webcam_publisher.py
@@ -12,27 +12,33 @@ class WebcamPublisher(Node):
         super().__init__("webcam_publisher")
         self.publisher_ = self.create_publisher(Image, "webcam_image", 10)
         self.bridge = CvBridge()
-        self.runtime_parameters = sl.RuntimeParameters()
         self.timer = self.create_timer(1.0/30.0, self.publish_webcam_image)
+        self.cap = cv2.VideoCapture(0)
+
 
     def publish_webcam_image(self):
-        cap = cv2.VideoCapture(0)
-        ret, frame = cap.read()
+        ret, frame = self.cap.read()
         if ret:
             ros_image = self.bridge.cv2_to_imgmsg(frame, "bgr8")
             self.publisher_.publish(ros_image)
         if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-        cap.release()
-        cv2.destroyAllWindows()
+            return
+    
+    def close(self):
+        self.cap.release()
+        cv2.destroyAllWindows() 
+
 
 
 def main(args=None):
     rclpy.init(args=args)
     webcam_publisher = WebcamPublisher()
-    webcam_publisher.publish_webcam_image()
-    rclpy.spin(webcam_publisher) #means run continously
-    rclpy.shutdown()
+    try:
+        rclpy.spin(webcam_publisher) #means run continously
+    except KeyboardInterrupt:
+        webcam_publisher.close()
+    finally:
+        rclpy.shutdown()
 
 
 if __name__ == "__main__":
