8fd545badf578b8be36d658ac8e10dfcbd3d4955
Robotic Lions <126622391+RoboticLions@users.noreply.github.com>
2024-09-29 20:21:30 +0000

    convert yolov8 messages

--- all_seaing_perception/all_seaing_perception/yolov8_node.py
+++ all_seaing_perception/all_seaing_perception/yolov8_node.py
@@ -1,20 +1,10 @@
-# Copyright (C) 2023  Miguel Ángel González Santamarta
+#!/usr/bin/env python3
+"""
+References:
+https://github.com/mgonzs13/yolov8_ros
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+"""
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
-from typing import List, Dict
 
 import rclpy
 from rclpy.node import Node
@@ -31,20 +21,14 @@ from ultralytics.engine.results import Boxes
 from ultralytics.engine.results import Masks
 from ultralytics.engine.results import Keypoints
 
+from all_seaing_interfaces.msg import LabeledBoundingBox2D, LabeledBoundingBox2DArray
+
 from sensor_msgs.msg import Image
-from yolov8_msgs.msg import Point2D
-from yolov8_msgs.msg import BoundingBox2D
-from yolov8_msgs.msg import Mask
-from yolov8_msgs.msg import KeyPoint2D
-from yolov8_msgs.msg import KeyPoint2DArray
-from yolov8_msgs.msg import Detection
-from yolov8_msgs.msg import DetectionArray
 from std_srvs.srv import SetBool
 
 
 class Yolov8Node(Node):
 
-    # Add self.model_path in __init__ later (See yolov5_detector)
     def __init__(self) -> None:
         super().__init__("yolov8_node")
 
@@ -80,7 +64,7 @@ class Yolov8Node(Node):
         self.yolo.fuse()
 
         # pubs
-        self._pub = self.create_publisher(DetectionArray, "detections", 10)
+        self._pub = self.create_publisher(LabeledBoundingBox2DArray, "bounding_boxes", 10)
 
         # subs
         self._sub = self.create_subscription(
@@ -100,93 +84,6 @@ class Yolov8Node(Node):
         res.success = True
         return res
 
-    def parse_hypothesis(self, results: Results) -> List[Dict]:
-
-        hypothesis_list = []
-
-        box_data: Boxes
-        for box_data in results.boxes:
-            hypothesis = {
-                "class_id": int(box_data.cls),
-                "class_name": self.yolo.names[int(box_data.cls)],
-                "score": float(box_data.conf)
-            }
-            hypothesis_list.append(hypothesis)
-
-        return hypothesis_list
-
-    def parse_boxes(self, results: Results) -> List[BoundingBox2D]:
-
-        boxes_list = []
-
-        box_data: Boxes
-        for box_data in results.boxes:
-
-            msg = BoundingBox2D()
-
-            # get boxes values
-            box = box_data.xywh[0]
-            msg.center.position.x = float(box[0])
-            msg.center.position.y = float(box[1])
-            msg.size.x = float(box[2])
-            msg.size.y = float(box[3])
-
-            # append msg
-            boxes_list.append(msg)
-
-        return boxes_list
-
-    def parse_masks(self, results: Results) -> List[Mask]:
-
-        masks_list = []
-
-        def create_point2d(x: float, y: float) -> Point2D:
-            p = Point2D()
-            p.x = x
-            p.y = y
-            return p
-
-        mask: Masks
-        for mask in results.masks:
-
-            msg = Mask()
-
-            msg.data = [create_point2d(float(ele[0]), float(ele[1]))
-                        for ele in mask.xy[0].tolist()]
-            msg.height = results.orig_img.shape[0]
-            msg.width = results.orig_img.shape[1]
-
-            masks_list.append(msg)
-
-        return masks_list
-
-    def parse_keypoints(self, results: Results) -> List[KeyPoint2DArray]:
-
-        keypoints_list = []
-
-        points: Keypoints
-        for points in results.keypoints:
-
-            msg_array = KeyPoint2DArray()
-
-            if points.conf is None:
-                continue
-
-            for kp_id, (p, conf) in enumerate(zip(points.xy[0], points.conf[0])):
-
-                if conf >= self.threshold:
-                    msg = KeyPoint2D()
-
-                    msg.id = kp_id + 1
-                    msg.point.x = float(p[0])
-                    msg.point.y = float(p[1])
-                    msg.score = float(conf)
-
-                    msg_array.data.append(msg)
-
-            keypoints_list.append(msg_array)
-
-        return keypoints_list
 
     def image_cb(self, msg: Image) -> None:
 
@@ -203,41 +100,27 @@ class Yolov8Node(Node):
             )
             results: Results = results[0].cpu()
 
-            if results.boxes:
-                hypothesis = self.parse_hypothesis(results)
-                boxes = self.parse_boxes(results)
+            # create labeled_bounding_box msgs
+            labeled_bouding_box_msgs = LabeledBoundingBox2DArray()
 
-            if results.masks:
-                masks = self.parse_masks(results)
+            for box_data in results.boxes:
 
-            if results.keypoints:
-                keypoints = self.parse_keypoints(results)
-
-            # create detection msgs
-            detections_msg = DetectionArray()
-
-            for i in range(len(results)):
-
-                aux_msg = Detection()
+                box_msg = LabeledBoundingBox2D()
 
                 if results.boxes:
-                    aux_msg.class_id = hypothesis[i]["class_id"]
-                    aux_msg.class_name = hypothesis[i]["class_name"]
-                    aux_msg.score = hypothesis[i]["score"]
+                    #self.yolo.names[int(box_data.cls)]
+                    box_msg.label = int(box_data.cls)
+                    box_msg.probability = float(box_data.conf)
+                    center_x, center_y, width, height = box_data.xywh
+                    box_msg.min_x = int(center_x - width/2)
+                    box_msg.max_x = int(center_x + width/2)
+                    box_msg.min_y = int(center_y - height/2)
+                    box_msg.max_y = int(center_y + height/2)
 
-                    aux_msg.bbox = boxes[i]
-
-                if results.masks:
-                    aux_msg.mask = masks[i]
-
-                if results.keypoints:
-                    aux_msg.keypoints = keypoints[i]
-
-                detections_msg.detections.append(aux_msg)
+                labeled_bouding_box_msgs.detections.append(box_msg)
 
             # publish detections
-            detections_msg.header = msg.header
-            self._pub.publish(detections_msg)
+            self._pub.publish(labeled_bouding_box_msgs)
 
 
 def main():
