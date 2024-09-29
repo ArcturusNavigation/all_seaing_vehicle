9f5cbf2b7c5c7b614dd770357249dc6fbb5e877d
Panagiotis Liampas <pliam1105@gmail.com>
2024-09-29 18:12:42 -0400

    Now publishes buoy pairs and waypoints, but can't visualize it on RViz to check if it's correct, need to figure that out

--- all_seaing_autonomy/all_seaing_autonomy/roboboat/WaypointFinder.py
+++ all_seaing_autonomy/all_seaing_autonomy/roboboat/WaypointFinder.py
@@ -12,7 +12,7 @@ class WaypointFinder(Node):
     def __init__(self):
         super().__init__("waypoint_finder")
         self.map_sub = self.create_subscription(
-            ObstacleMap, "labeled_map", self.map_cb, 10
+            ObstacleMap, "/obstacle_map/labeled", self.map_cb, 10
         )
         self.odometry_sub = self.create_subscription(Odometry, "/odometry/filtered", self.odometry_cb, 10)
         self.buoy_pair_pub = self.create_publisher(BuoyPairArray, "buoy_pairs", 10)
@@ -27,13 +27,13 @@ class WaypointFinder(Node):
         return vec[0] ** 2 + vec[1] ** 2
 
     def get_closest_to(self, source, points):
-        return min(
-            points,
-            key=lambda point: self.norm_squared(source, point)
-        )
+        return min(points, key=lambda point: self.norm_squared(source, point))
 
     def midpoint(self, vec1, vec2):
         return ((vec1[0] + vec2[0]) / 2, (vec1[1] + vec2[1]) / 2)
+    
+    def midpoint_pair(self, pair):
+        return self.midpoint(pair[0], pair[1])
     
     def split_buoys(self, obstacles):
         green_bouy_points = []
@@ -44,12 +44,27 @@ class WaypointFinder(Node):
             elif obstacle.label == 1:
                 red_bouy_points.append(obstacle)
         return green_bouy_points, red_bouy_points
+    
+    def obs_to_pos(self, obs):
+        return [(ob.global_point.point.x, ob.global_point.point.y) for ob in obs]
+    
+    def obs_to_pos_label(self, obs):
+        return [(ob.global_point.point.x, ob.global_point.point.y, ob.label) for ob in obs]
 
     def setup_buoys(self):
-        self.get_logger().warning("Setting up starting buoys!")
-        green_buoys, red_buoys = zip(*[(ob.global_point.point.x, ob.global_point.point.y) for ob in self.split_buoys(self, self.obstacles) if ob.local_point.point.x > 0])
+        self.get_logger().info("Setting up starting buoys!")
+        self.get_logger().info("obstacles: "+str(self.obs_to_pos_label(self.obstacles)))
+        green_init, red_init = self.split_buoys(self.obstacles)
+        obstacles_in_front = lambda obs: [(ob.global_point.point.x, ob.global_point.point.y) for ob in obs if ob.local_point.point.x > 0]
+        green_buoys, red_buoys = obstacles_in_front(green_init), obstacles_in_front(red_init)
+        self.get_logger().info("initial red buoys: "+str(red_buoys)+", green buoys: "+str(green_buoys))
+        if(len(red_buoys)==0 or len(green_buoys)==0):
+            self.get_logger().warning("No starting buoy pairs!")
+            return False
         self.starting_buoys = (self.get_closest_to((0,0),red_buoys), self.get_closest_to((0,0),green_buoys))
         self.pair_to = self.starting_buoys
+        self.get_logger().info("starting buoys: "+str(self.starting_buoys))
+        return True
     
     def ccw(self, a, b, c):
         """Check if the points a, b, c are counterclockwise, in this order, and return True, otherwise return False"""
@@ -67,10 +82,17 @@ class WaypointFinder(Node):
     def next_pair(self, prev_pair, red, green):
         """Find the next buoy pair (red left, green right) from the previous pair,
         by checking the closes one to the middle of the previous buoy pair that's in front of the pair"""
-        return (self.get_closest_to(self.midpoint(prev_pair), self.filter_front_buoys(red)), self.get_closest_to(self.midpoint(prev_pair), self.filter_front_buoys(green)))
-
+        return (self.get_closest_to(self.midpoint_pair(prev_pair), self.filter_front_buoys(prev_pair, red)), self.get_closest_to(self.midpoint(prev_pair), self.filter_front_buoys(prev_pair, green)))
+    
+    def pair_to_pose(self, pair):
+        return Pose(position = Point(x = pair[0], y = pair[1]))
+    
     def generate_waypoints(self):
-        green_buoys, red_buoys = self.split_buoys(self, self.obstacles)
+        self.get_logger().info("obstacles: "+str(self.obs_to_pos_label(self.obstacles)))
+        green_buoys, red_buoys = self.split_buoys(self.obstacles)
+        green_buoys = self.obs_to_pos(green_buoys)
+        red_buoys = self.obs_to_pos(red_buoys)
+        self.get_logger().info("red buoys: "+str(red_buoys)+", green buoys: "+str(green_buoys))
         # RED BUOYS LEFT, GREEN RIGHT
 
         if len(green_buoys) == 0 or len(red_buoys) == 0:
@@ -81,28 +103,41 @@ class WaypointFinder(Node):
         
         # Check if we passed that pair of buoys, then move on to the next one
         if self.ccw(self.pair_to[0],self.pair_to[1], self.robot_pos):
-            self.pair_to = self.next_pair(self.pair_to)
+            try:
+                self.pair_to = self.next_pair(self.pair_to, red_buoys, green_buoys)
+            except:
+                self.get_logger().warning("No next buoy pair to go to!")
 
         buoy_pairs = [self.pair_to]
-        waypoints = [self.midpoint(self.pair_to)]
+        waypoints = [self.midpoint_pair(self.pair_to)]
 
         # will terminate if we run out of either of the points
-        while len(green_buoys) > 0 and len(red_buoys) > 0:
-            last_waypoint = self.midpoint(buoy_pairs[-1])
-            buoy_pairs.append(self.next_pair(last_waypoint))
-            waypoints.append(self.midpoint(buoy_pairs[-1]))
+        while True:
+            last_waypoint = self.midpoint_pair(buoy_pairs[-1])
+            try:
+                buoy_pairs.append(self.next_pair(last_waypoint))
+                waypoints.append(self.midpoint(buoy_pairs[-1]))
+            except:
+                break
         
-        waypoint_arr = WaypointArray(waypoints = [Waypoint(Pose(position = wpt), radius = math.sqrt(self.norm_squared(pair[0], pair[1])) - self.safe_margin) for wpt, pair in zip(waypoints, buoy_pairs)])
-        buoy_pairs = BuoyPairArray(pairs = [BuoyPair(left = pair[0], right = pair[1], waypoint = waypoint) for pair, waypoint in zip(buoy_pairs, waypoint_arr)])
+        waypoint_arr = WaypointArray(waypoints = [Waypoint(point = self.pair_to_pose(wpt), radius = math.sqrt(self.norm_squared(pair[0], pair[1])) - self.safe_margin) for wpt, pair in zip(waypoints, buoy_pairs)])
+        buoy_pairs = BuoyPairArray(pairs = [BuoyPair(left = self.pair_to_pose(pair[0]), right = self.pair_to_pose(pair[1]), waypoint = waypoint) for pair, waypoint in zip(buoy_pairs, waypoint_arr.waypoints)])
         self.buoy_pair_pub.publish(buoy_pairs)
         self.waypoint_pub.publish(waypoint_arr)
+        self.get_logger().info("buoy pairs: "+str(buoy_pairs)+", waypoints: "+str(waypoint_arr))
 
     def map_cb(self, msg):
         self.obstacles = msg.obstacles
+        self.get_logger().info("obstacles: "+str(self.obs_to_pos_label(self.obstacles)))
+        
+        success = False
         if self.first_map:
-            self.setup_buoys()
-            self.first_map = False
-        self.generate_waypoints()
+            success = self.setup_buoys()
+            self.first_map = not success
+        else:
+            success = True
+        if success:
+            self.generate_waypoints()
         
     def odometry_cb(self, msg):
         self.robot_pos = (msg.pose.pose.position.x, msg.pose.pose.position.y)
