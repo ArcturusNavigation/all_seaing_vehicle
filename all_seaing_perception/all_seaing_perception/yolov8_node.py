0026049b17ecce268b81d7d69b6f277f1948eaa4
Arthur De Los Santos <arthurdls04@gmail.com>
2024-10-06 16:05:14 -0400

    allow yolov8_node to access models folder

--- all_seaing_perception/all_seaing_perception/yolov8_node.py
+++ all_seaing_perception/all_seaing_perception/yolov8_node.py
@@ -13,13 +13,13 @@ ros2 run all_seaing_perception yolov8_node --ros-args \
   -p image_topic:=your_custom_image_topic
 """
 
-
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile
 from rclpy.qos import QoSHistoryPolicy
 from rclpy.qos import QoSDurabilityPolicy
 from rclpy.qos import QoSReliabilityPolicy
+
 
 from cv_bridge import CvBridge
 
@@ -28,6 +28,9 @@ from ultralytics.engine.results import Results
 from ultralytics.engine.results import Boxes
 
 from all_seaing_interfaces.msg import LabeledBoundingBox2D, LabeledBoundingBox2DArray
+
+import os
+from ament_index_python.packages import get_package_share_directory
 
 from sensor_msgs.msg import Image
 from std_srvs.srv import SetBool
@@ -38,25 +41,31 @@ class Yolov8Node(Node):
     def __init__(self) -> None:
         super().__init__("yolov8_node")
 
-        # params
+        # Declare parameters
         self.declare_parameter("model", "yolov8m.pt")
-        model = self.get_parameter(
-            "model").get_parameter_value().string_value
-
         self.declare_parameter("device", "cuda:0")
-        self.device = self.get_parameter(
-            "device").get_parameter_value().string_value
-
         self.declare_parameter("threshold", 0.5)
-        self.threshold = self.get_parameter(
-            "threshold").get_parameter_value().double_value
-
         self.declare_parameter("enable", True)
-        self.enable = self.get_parameter(
-            "enable").get_parameter_value().bool_value
+        self.declare_parameter("image_topic", "image_raw") 
+        self.declare_parameter("image_reliability", QoSReliabilityPolicy.BEST_EFFORT)
 
-        self.declare_parameter("image_reliability",
-                               QoSReliabilityPolicy.BEST_EFFORT)
+        # Get parameters
+        model_name = self.get_parameter("model").get_parameter_value().string_value
+        self.device = self.get_parameter("device").get_parameter_value().string_value
+        self.threshold = self.get_parameter("threshold").get_parameter_value().double_value
+        self.enable = self.get_parameter("enable").get_parameter_value().bool_value
+        image_topic = self.get_parameter("image_topic").get_parameter_value().string_value
+
+        # Get the model's path
+        package_path = get_package_share_directory('all_seaing_perception')
+        model_path = os.path.join(package_path, 'models', model_name)
+
+        # Initialize YOLO model
+        self.cv_bridge = CvBridge()
+        self.yolo = YOLO(model_path)
+        self.yolo.fuse()
+
+        # Setup QoS profile
         image_qos_profile = QoSProfile(
             reliability=self.get_parameter(
                 "image_reliability").get_parameter_value().integer_value,
@@ -65,23 +74,11 @@ class Yolov8Node(Node):
             depth=1
         )
 
-        self.cv_bridge = CvBridge()
-        self.yolo = YOLO(model)
-        self.yolo.fuse()
-
-        # pubs
+        # Publisher and Subscriber
         self._pub = self.create_publisher(LabeledBoundingBox2DArray, "bounding_boxes", 10)
+        self._sub = self.create_subscription(Image, image_topic, self.image_cb, image_qos_profile)
 
-        # subs
-        self.declare_parameter("image_topic", "image_raw") 
-        image_topic = self.get_parameter("image_topic").get_parameter_value().string_value
-
-        self._sub = self.create_subscription(
-            Image, image_topic, self.image_cb,
-            image_qos_profile
-        )
-
-        # services
+        # Service for enabling/disabling
         self._srv = self.create_service(SetBool, "enable", self.enable_cb)
 
     def enable_cb(
