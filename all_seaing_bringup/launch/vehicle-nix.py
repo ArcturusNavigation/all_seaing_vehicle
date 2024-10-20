94cef8c4b0e19a55cae2f26856601406023e663f
Ananth Venkatesh <dev.quantum9innovation@gmail.com>
2024-10-20 15:37:25 -0400

    build(nix): create variant launch files

---
+++ all_seaing_bringup/launch/vehicle-nix.py
@@ -0,0 +1,140 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+import launch_ros
+import os
+import yaml
+
+
+def launch_setup(context, *args, **kwargs):
+
+    bringup_prefix = get_package_share_directory("all_seaing_bringup")
+    driver_prefix = get_package_share_directory("all_seaing_driver")
+
+    robot_localization_params = os.path.join(
+        bringup_prefix, "config", "localization", "localize_real.yaml"
+    )
+    locations_file = os.path.join(
+        bringup_prefix, "config", "localization", "locations.yaml"
+    )
+
+    location = context.perform_substitution(LaunchConfiguration("location"))
+
+    ekf_node = launch_ros.actions.Node(
+        package="robot_localization",
+        executable="ekf_node",
+        parameters=[robot_localization_params],
+    )
+
+    with open(locations_file, 'r') as f:
+        locations = yaml.safe_load(f)
+    lat = locations[location]["lat"]
+    lon = locations[location]["lon"]
+    navsat_node = launch_ros.actions.Node(
+        package="robot_localization",
+        executable="navsat_transform_node",
+        remappings=[("gps/fix", "/mavros/global_position/raw/fix")],
+        parameters=[
+            robot_localization_params,
+            {"datum": [lat, lon, 0.0]},
+        ],
+    )
+
+    controller_node = launch_ros.actions.Node(
+        package="all_seaing_controller",
+        executable="xdrive_controller.py",
+        parameters=[
+            {
+                "front_right_xy": [0.5, -0.5],
+                "back_left_xy": [-0.5, 0.5],
+                "front_left_xy": [0.5, 0.5],
+                "back_right_xy": [-0.5, -0.5],
+                "thruster_angle": 45.0,
+                "drag_constants": [5.0, 5.0, 40.0],
+                "output_range": [1100.0, 1900.0],
+                "smoothing_factor": 0.8,
+            }
+        ],
+    )
+
+    thrust_commander_node = launch_ros.actions.Node(
+        package="all_seaing_driver",
+        executable="thrust_commander.py",
+        parameters=[
+            {
+                "front_right_port": 2,
+                "front_left_port": 3,
+                "back_right_port": 4,
+                "back_left_port": 5,
+            }
+        ],
+    )
+
+    control_mux = launch_ros.actions.Node(
+        package="all_seaing_controller",
+        executable="control_mux.py",
+    )
+
+    controller_server = launch_ros.actions.Node(
+        package="all_seaing_controller",
+        executable="controller_server.py",
+        parameters=[
+            {"global_frame_id": "odom"},
+            {"Kpid_x": [1.0, 0.0, 0.0]},
+            {"Kpid_y": [1.0, 0.0, 0.0]},
+            {"Kpid_theta": [1.0, 0.0, 0.0]},
+            {"max_vel": [2.0, 1.0, 0.8]},
+        ],
+        output="screen",
+    )
+
+    waypoint_sender = launch_ros.actions.Node(
+        package="all_seaing_navigation",
+        executable="waypoint_sender.py",
+        parameters=[
+            {"xy_threshold": 1.0},
+            {"theta_threshold": 5.0},
+        ],
+        output="screen",
+    )
+
+    rover_lora_controller = launch_ros.actions.Node(
+        package="all_seaing_driver",
+        executable="rover_lora_controller.py",
+        output="screen",
+    )
+
+    mavros_ld = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                driver_prefix,
+                "/launch/mavros.launch.py",
+            ]
+        ),
+        launch_arguments={
+            "port": "/dev/ttyACM0",
+        }.items(),
+    )
+
+    return [
+        ekf_node,
+        navsat_node,
+        control_mux,
+        controller_node,
+        controller_server,
+        waypoint_sender,
+        rover_lora_controller,
+        thrust_commander_node,
+        mavros_ld,
+    ]
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument("location", default_value="boathouse"),
+            OpaqueFunction(function=launch_setup),
+        ]
+    )
