b961579f3c7cbd89915bbb5897907b8593afd50d
popkarsd <brendon.jiang1234567890@gmail.com>
2024-11-15 15:21:23 -0500

    first version of working mapping_server

--- all_seaing_navigation/all_seaing_navigation/mapping_server/mapping_server.py
+++ all_seaing_navigation/all_seaing_navigation/mapping_server/mapping_server.py
@@ -3,8 +3,8 @@ import rclpy
 from rclpy.node import Node
 from scipy.spatial import ConvexHull
 # from utils import GeometryPrimitives
-
-from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import Pose
+from nav_msgs.msg import OccupancyGrid, MapMetaData
 from all_seaing_interfaces.msg import ObstacleMap
 
 
@@ -21,27 +21,37 @@ class PathPlan(Node):
             ObstacleMap, self.labeled_map_topic, self.hulls_update_cb, 10)
 
         # Publishers for path and PoseArray
-        self.grid_pub = self.create_publisher(OccupancyGrid, "occupancy_grid", 10)
+        self.grid_pub = self.create_publisher(OccupancyGrid, "occupancy_grid", 20)
 
         # Local variables
         self.labeled_map = ObstacleMap()
 
         # Actual location of competition (Nathan Benderson Park) has an 
-        # interior square area of around 117m * 117m. We use a 200m * 200m square.
-        # Occupancy Grid indexing: index = X+Y*width
+        #     interior square area of around 117m * 117m. We use a 200m * 200m square.
         self.grid = OccupancyGrid()
-        self.grid.header.frame_id = 'grid'
+        self.grid.header.frame_id = 'map'
+
+        self.grid.info = MapMetaData()
         self.grid.info.width = 2000
         self.grid.info.height = 2000
         self.grid.info.resolution = 0.1
 
+        self.grid.info.origin = Pose()
+        self.grid.info.origin.position.x = 0.0
+        self.grid.info.origin.position.y = 0.0
+        self.grid.info.origin.position.z = 0.0
+
+
+        self.grid.data = [-1] * self.grid.info.width * self.grid.info.height
+
         # Position of ship relative to global origin
-        self.ship_pos = (0,0)
+        # TO DO: receive and set the position of the ship somewhere
+        self.ship_pos = (10,42)
         self.lidar_rad = 10
 
 
         # Active cells in row major order            
-        self.active_cells = [[False] * self.grid.info.width * self.grid.info.height]
+        self.active_cells = [False] * self.grid.info.width * self.grid.info.height
         self.get_logger().info("Initialized Mapping Server")
 
     def ccw(self,A,B,C):
@@ -61,93 +71,83 @@ class PathPlan(Node):
 
     def world_to_grid(self, x, y):
         """Convert world coordinates to grid coordinates."""
-        origin = self.map_info.origin.position
-        resolution = self.map_info.resolution
+        origin = self.grid.info.origin.position
+        resolution = self.grid.info.resolution
         gx = int((x - origin.x) / resolution)
         gy = int((y - origin.y) / resolution)
         return (gx, gy)
 
     def grid_to_world(self, gx, gy):
         """Convert grid coordinates back to world coordinates."""
-        origin = self.map_info.origin.position
-        resolution = self.map_info.resolution
+        origin = self.grid.info.origin.position
+        resolution = self.grid.info.resolution
         x = gx * resolution + origin.x
         y = gy * resolution + origin.y
         return x, y
 
     def find_active_cells(self):
-        if len(self.labeled_map.obstacles) == 0:
-            for x in range(0, self.grid.width):
-                for y in range(0, self.grid.height):
-                    self.active_cells[x+y*self.grid.width] = True
-                    return
         """
         Find the current active cells that the sensors can see, and mark them as active
         Mark cells inactive if behind obstacles
         """
-
-        ob = self.labeled_map.obstacles[0]
-        self.ship_pos = (ob.local_point.point.x-ob.global_point.point.x,
-                    ob.local_point.point.y-ob.global_point.point.y)
         all_modified_hulls = []
-        all_hulls = []
+        all_mpolys = []
         for obstacle in self.labeled_map.obstacles:
             gchull = obstacle.global_chull.polygon.points
-            polygon = [(point.x, point.y) for point in gchull]
-            all_hulls.append(ConvexHull(polygon))
-            polygon.append(self.ship_pos)
-            modified_hull = ConvexHull(polygon)
-            all_modified_hulls.append(modified_hull)
+            m_polygon = [(point.x, point.y) for point in gchull]
+            m_polygon.append(self.ship_pos)
+            all_mpolys.append(m_polygon)
+            all_modified_hulls.append(ConvexHull(m_polygon))
 
         #test if cells are active
         #O(W * H * |hulls|) (very slow)
-        for x in range(0, self.grid.width):
-            for y in range(0, self.grid.height):
-                worldx, worldy = self.grid_to_world(x), self.grid_to_world(y)
+        self.get_logger().info("Started finding active cells")
+        for x in range(0, self.grid.info.width):
+            for y in range(0, self.grid.info.height):
+                worldx, worldy = self.grid_to_world(x,y)
+                rad = (worldx-self.ship_pos[0])**2 + (worldy-self.ship_pos[1])**2
                 if (worldx-self.ship_pos[0])**2 + (worldy-self.ship_pos[1])**2 > self.lidar_rad**2:
-                    self.active_cells[x + y * self.grid.width] = False
+                    self.active_cells[x + y * self.grid.info.width] = False
                     continue
-
-                self.active_cells[x + y * self.grid.width] = True
-                for hull,mhull in zip(all_hulls,all_modified_hulls):
-                    continue_flag = False
-                    for line_indices in hull.simplices:
-                        if self.intersect(line_indices[0], line_indices[1],
-                                         worldx-self.ship_pos[0], worldy-self.ship_pos[1]):
-                            continue_flag = True
-                            break
-                    if continue_flag: 
-                        continue
-
+                self.active_cells[x + y * self.grid.info.width] = True
+                if len(self.labeled_map.obstacles) == 0:
+                    continue
+                for mhull, mpoly in zip(all_modified_hulls, all_mpolys):
                     # Check if cell is behind convex hull
                     ctr = 0
                     for line_indices in mhull.simplices: #indices of points that form a line
-                        ctr += (self.intersect(line_indices[0], line_indices[1],
-                                                (worldx,worldy), (self.grid.info.width+0.00007,self.grid.info.height)))
-                    if ctr%2 == 0:
-                        self.active_cells[x + y * self.grid.width] = False
+                        ctr += (self.intersect(mpoly[line_indices[0]], mpoly[line_indices[1]],
+                                                (int(worldx),int(worldy)), (self.ship_pos[0], self.ship_pos[1])))
+                    if ctr == 2:
+                        continue
+                    if ctr > 2:
+                        self.active_cells[x + y * self.grid.info.width] = False
                         break
+
+        self.get_logger().info("Found active cells")
+        self.modify_probability()
 
     def modify_probability(self):
         """Decay or increase probability of obstacle in active cells based on sensor observations"""
-        for x in range(0, self.grid.width):
-            for y in range(0, self.grid.height):
-                self.grid[x+y*self.grid.width] = 100 if self.active_cells[x+y*self.grid.width] else 0
+        for x in range(0, self.grid.info.width):
+            for y in range(0, self.grid.info.height):
+                if self.active_cells[x + y*self.grid.info.width]:
+                    self.grid.data[x+y*self.grid.info.width] = 100
+                else:
+                    self.grid.data[x+y*self.grid.info.width] = 0
+        self.publish_grid()
 
     
     def hulls_update_cb(self, msg):
         self.labeled_map = msg
-        self.get_logger().debug("Mapping server: Intialized obstacles" if self.labeled_map is None else "Mapping server: Updated obstacles")
-    
+        self.get_logger().info("Mapping server: Intialized obstacles" if self.labeled_map is None else "Mapping server: Updated obstacles")
+        self.find_active_cells()
+
     def publish_grid(self):
         """Publish grid as nav_msgs/Grid"""
         self.grid.header.stamp = self.get_clock().now().to_msg()
         self.grid_pub.publish(self.grid)
-        self.get_logger().debug("Mapping Server: Published Occupancy Grid")
-
-
-    # def pose_to_string(self, pos):
-    #     return f"{{{pos.position.x}, {pos.position.y}, {pos.position.z}}}"
+        self.get_logger().info("Mapping Server: Published Occupancy Grid")
 
 def main(args=None):
     rclpy.init(args=args)
