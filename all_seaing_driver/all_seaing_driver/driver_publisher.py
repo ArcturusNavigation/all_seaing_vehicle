2b45ce31e4a2b003fa22ed88f50836a60ed67deb
jiamei <jiamei@mit.edu>
2025-02-04 21:15:18 -0500

    added estop control

--- all_seaing_driver/all_seaing_driver/driver_publisher.py
+++ all_seaing_driver/all_seaing_driver/driver_publisher.py
@@ -1,36 +1,36 @@
+#!/usr/bin/env python3
+
 import rclpy
 from rclpy.node import Node # imports Node class from ros2 packages
-from std_msgs.msg import Int64, Joy
-from driver_lib import ESTOP
+from std_msgs.msg import UInt8
+from sensor_msgs.msg import Joy
+from all_seaing_driver.driver_lib import ESTOP
 import serial
 import time
 
 from all_seaing_interfaces.msg import ControlOption
 from geometry_msgs.msg import Twist
-from all_seaing_interfaces.msg import Heartbeat
 
-HEART_RATE = 1
+TIMER_RATE = 1
 
 class DriverPublisher(Node):
     def __init__(self):
         super().__init__("driver_publisher")
 
-        # initialize ser and add; TODO: need to determine port
-        self.ser = serial.Serial("COM16", 115200, timeout = 1)
-        self.estop = ESTOP(self.ser)
-
         self.declare_parameter("joy_x_scale", 2.0)
         self.declare_parameter("joy_y_scale", -1.0)
         self.declare_parameter("joy_ang_scale", -0.8)
+        self.declare_parameter("serial_port", "COM16")
 
         self.joy_x_scale = self.get_parameter("joy_x_scale").value
         self.joy_y_scale = self.get_parameter("joy_y_scale").value
         self.joy_ang_scale = self.get_parameter("joy_ang_scale").value
+        self.ser = serial.Serial(self.get_parameter("serial_port").value, 115200, timeout = 1)
+        self.estop = ESTOP(self.ser)
 
-        self.heartbeat_message = Heartbeat()
-        self.heartbeat_publisher = self.create_publisher(Heartbeat, "heartbeat", 10)
-        self.heartbeat_message.in_teleop = True
-        self.heartbeat_message.e_stopped = False
+        self.estop_msg = UInt8()
+        self.estop_pub = self.create_publisher(UInt8, "estop_stat", 10)
+        self.estop_msg.e_stopped = 0
 
         self.enter_held = False
 
@@ -38,14 +38,14 @@ class DriverPublisher(Node):
             ControlOption, "control_options", 10
         )
         self.joy_control_sub = self.create_subscription(
-            Joy, "/joy", self.keyboard_callback, 10
+            Joy, "/joy", self.joystick_callback, 10
         )
-        self.heartbeat_timer = self.create_timer(HEART_RATE, self.beat_heart)
+        self.estop_stat_timer = self.create_timer(TIMER_RATE, self.send_estop_stat)
 
-        self.get_logger().info("Starting onshore node, teleop enabled")
+        self.get_logger().info("Starting driver node, teleop enabled")
 
-    def beat_heart(self):
-        self.heartbeat_publisher.publish(self.heartbeat_message)
+    def send_estop_stat(self):
+        self.estop_pub.publish(self.estop_msg)
 
     def send_controls(self, x, y, angular):
         control_option = ControlOption()
@@ -55,29 +55,14 @@ class DriverPublisher(Node):
         control_option.twist.angular.z = angular
         self.control_option_pub.publish(control_option)
 
-    def keyboard_callback(self, msg):
-        if self.heartbeat_message.e_stopped:
-            self.get_logger().fatal("ASV is e-stopped!")
+    def joystick_callback(self, msg):
+        self.estop_msg.e_stopped = self.estop.estop()
+
+        if self.estop_msg.e_stopped == 1:
+            self.get_logger().info("E-stop activated.")
+            self.estop_pub.publish(self.estop_msg)
             return
-
-        if msg.buttons[0]:  # msg.buttons[0] = space bar --> e-stop (reference config)
-            self.get_logger().info("E-stop pressed!")
-            self.heartbeat_message.e_stopped = True
-            self.heartbeat_publisher.publish(self.heartbeat_message)
-            return
-
-        if msg.buttons[1]:  # msg.buttons[1] = return key (reference config)
-            if not self.enter_held:
-                self.enter_held = True
-                self.heartbeat_message.in_teleop = not self.heartbeat_message.in_teleop
-                self.heartbeat_publisher.publish(self.heartbeat_message)
-                self.get_logger().info(
-                    f"Toggled teleop (now {self.heartbeat_message.in_teleop})"
-                )
-        elif self.enter_held:
-            self.enter_held = False
-
-        if self.heartbeat_message.in_teleop:
+        else:
             self.send_controls(
                 msg.axes[1] * self.joy_x_scale,
                 msg.axes[0] * self.joy_y_scale,
@@ -87,7 +72,7 @@ class DriverPublisher(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    node = OnshoreNode()
+    node = DriverPublisher()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
