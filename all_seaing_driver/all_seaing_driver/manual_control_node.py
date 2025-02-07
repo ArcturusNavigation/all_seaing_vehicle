bcfd4901a2a5bbfd2ee106b155eed25ab07aff8c
jiamei <jiamei@mit.edu>
2025-02-06 20:40:26 -0500

    Manual control of robot completed

--- all_seaing_driver/all_seaing_driver/manual_control_node.py
+++ all_seaing_driver/all_seaing_driver/manual_control_node.py
@@ -3,7 +3,7 @@
 import rclpy
 from rclpy.node import Node # imports Node class from ros2 packages
 from std_msgs.msg import UInt8
-from all_seaing_driver.driver_lib import ESTOP
+from all_seaing_driver.driver_library import ESTOP
 import serial
 import time
 
--- all_seaing_driver/all_seaing_driver/object_delivery_node.py
+++
@@ -1,9 +0,0 @@
-import rclpy
-from rclpy.node import Node # imports Node class from ros2 packages
-from std_msgs.msg import UInt8
-from all_seaing_driver.driver_lib import ESTOP
-import serial
-import time
-
-from all_seaing_interfaces.msg import ControlOption
-from geometry_msgs.msg import Twist
