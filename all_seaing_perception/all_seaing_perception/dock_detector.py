57ccce4b061d5ae019ccf3494c2e84182269fd28
Sasha <sashafefelova04@gmail.com>
2025-02-16 15:21:23 -0500

    working on dock detection node

--- all_seaing_perception/all_seaing_perception/dock_detector.py
+++ all_seaing_perception/all_seaing_perception/dock_detector.py
@@ -25,24 +25,76 @@ class DockDetector(Node):
         )
         
         # self.declare_parameter("lidar_point_cloud", "")
-        self.declare_parameter('dock_point_cloud_file', '')
-        dock_point_cloud_file = self.get_parameter('dock_point_cloud_file').value
-        self.known_dock_pc = None
+        # self.declare_parameter('dock_point_cloud_file', '')
+        # dock_point_cloud_file = self.get_parameter('dock_point_cloud_file').value
+
+        self.known_dock_pc, _ = read("point_clouds/roboboat_dock.ply", False, False, False)
         self.lidar_point_cloud = None
         self.robot_pos = (0, 0)
         self.timer = self.create_timer(1, self.detect_dock)
 
-        vertices, _ = read(dock_point_cloud_file, False, False, False)
-        self.known_dock_pc = vertices
-
+        # vertices, _ = read(dock_point_cloud_file, False, False, False)
+        # self.known_dock_pc = vertices
+    def nearest_neighbor(src, dst):
+        '''
+        Find the nearest (Euclidean) neighbor in dst for each point in src
+        Input:
+            src: Nxm array of points
+            dst: Nxm array of points
+        Output:
+            distances: Euclidean distances of the nearest neighbor
+            indices: dst indices of the nearest neighbor
+        '''
+        assert src.shape == dst.shape
+        neigh = NearestNeighbors(n_neighbors=1)
+        neigh.fit(dst)
+        distances, indices = neigh.kneighbors(src, return_distance=True)
+        return distances.ravel(), indices.ravel()
+    def best_fit_transform(A, B):
+        '''
+        Calculates the least-squares best-fit transform that maps corresponding points A to B in m spatial dimensions
+        Input:
+        A: Nxm numpy array of corresponding points
+        B: Nxm numpy array of corresponding points
+        Returns:
+        T: (m+1)x(m+1) homogeneous transformation matrix that maps A on to B
+        R: mxm rotation matrix
+        t: mx1 translation vector
+        '''
+        assert A.shape == B.shape
+        # get number of dimensions
+        m = A.shape[1]
+        # translate points to their centroids
+        centroid_A = np.mean(A, axis=0)
+        centroid_B = np.mean(B, axis=0)
+        AA = A - centroid_A
+        BB = B - centroid_B
+        # rotation matrix
+        H = np.dot(AA.T, BB)
+        U, S, Vt = np.linalg.svd(H)
+        R = np.dot(Vt.T, U.T)
+        # special reflection case
+        if np.linalg.det(R) < 0:
+            Vt[m-1,:] *= -1
+        R = np.dot(Vt.T, U.T)
+        # translation
+        t = centroid_B.T - np.dot(R,centroid_A.T)
+        # homogeneous transformation
+        T = np.identity(m+1)
+        T[:m, :m] = R
+        T[:m, m] = t
+        return T, R, t
     def get_point_cloud_transform(self, point_cloud_1, point_cloud_2):
         # Convert 
         # use ICP here
         # return transform and confidence
-        return self.icp(point_cloud_1_np, point_cloud_2_np)
+        return self.icp(point_cloud_1, point_cloud_2)
 
     def detect_dock(self):
+        self.get_logger().info('DETECT DOCK')
+
         transform, distances, _ = self.get_point_cloud_transform(self.known_dock_pc, self.lidar_point_cloud)
+        self.get_logger().info('got point cloud transform')
         # self.lidar_point_cloud_flat = # flatten
         # self.get_point_cloud_transform(__, __)
         # publish something
@@ -76,9 +128,9 @@ class DockDetector(Node):
         prev_error = 0
         for i in range(max_iterations):
             # find the nearest neighbors between the current source and destination points
-            distances, indices = nearest_neighbor(src[:m,:].T, dst[:m,:].T)
+            distances, indices = self.nearest_neighbor(src[:m,:].T, dst[:m,:].T)
             # compute the transformation between the current source and nearest destination points
-            T,_,_ = best_fit_transform(src[:m,:].T, dst[:m,indices].T)
+            T,_,_ = self.best_fit_transform(src[:m,:].T, dst[:m,indices].T)
             # update the current source
             src = np.dot(T, src)
             # check error
@@ -87,57 +139,10 @@ class DockDetector(Node):
                 break
             prev_error = mean_error
         # calculate final transformation
-        T,_,_ = best_fit_transform(A, src[:m,:].T)
+        T,_,_ = self.best_fit_transform(A, src[:m,:].T)
         return T, distances, i
-    def best_fit_transform(A, B):
-        '''
-        Calculates the least-squares best-fit transform that maps corresponding points A to B in m spatial dimensions
-        Input:
-        A: Nxm numpy array of corresponding points
-        B: Nxm numpy array of corresponding points
-        Returns:
-        T: (m+1)x(m+1) homogeneous transformation matrix that maps A on to B
-        R: mxm rotation matrix
-        t: mx1 translation vector
-        '''
-        assert A.shape == B.shape
-        # get number of dimensions
-        m = A.shape[1]
-        # translate points to their centroids
-        centroid_A = np.mean(A, axis=0)
-        centroid_B = np.mean(B, axis=0)
-        AA = A - centroid_A
-        BB = B - centroid_B
-        # rotation matrix
-        H = np.dot(AA.T, BB)
-        U, S, Vt = np.linalg.svd(H)
-        R = np.dot(Vt.T, U.T)
-        # special reflection case
-        if np.linalg.det(R) < 0:
-        Vt[m-1,:] *= -1
-        R = np.dot(Vt.T, U.T)
-        # translation
-        t = centroid_B.T - np.dot(R,centroid_A.T)
-        # homogeneous transformation
-        T = np.identity(m+1)
-        T[:m, :m] = R
-        T[:m, m] = t
-        return T, R, t
-    def nearest_neighbor(src, dst):
-        '''
-        Find the nearest (Euclidean) neighbor in dst for each point in src
-        Input:
-            src: Nxm array of points
-            dst: Nxm array of points
-        Output:
-            distances: Euclidean distances of the nearest neighbor
-            indices: dst indices of the nearest neighbor
-        '''
-        assert src.shape == dst.shape
-        neigh = NearestNeighbors(n_neighbors=1)
-        neigh.fit(dst)
-        distances, indices = neigh.kneighbors(src, return_distance=True)
-        return distances.ravel(), indices.ravel()
+    
+    
     def odometry_cb(self, msg):
         """
         Update the stored robot's position based on the odometry messages
@@ -149,7 +154,7 @@ class DockDetector(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    dock_detector = DockDetecor()
+    dock_detector = DockDetector()
     rclpy.spin(dock_detector)
     dock_detector.destroy_node()
     rclpy.shutdown()
