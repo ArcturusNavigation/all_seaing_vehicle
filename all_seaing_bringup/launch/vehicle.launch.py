47839c547c1c486a821906b0c73689079f283111
Panagiotis Liampas <pliam1105@gmail.com>
2025-07-26 11:07:15 -0400

    last night changes, ports and flipped joystick angles

--- all_seaing_bringup/launch/vehicle.launch.py
+++ all_seaing_bringup/launch/vehicle.launch.py
@@ -577,7 +577,7 @@ def launch_setup(context, *args, **kwargs):
             ]
         ),
         launch_arguments={
-            "port": "/dev/ttyACM0",
+            "port": "/dev/ttyACM0"
         }.items(),
         condition=UnlessCondition(use_bag),
     )
--- all_seaing_driver/all_seaing_driver/central_hub_ros.py
+++ all_seaing_driver/all_seaing_driver/central_hub_ros.py
@@ -94,8 +94,8 @@ class CentralHubROS(Node):
     def estop_cb(self, _, response):
         response.success = True
         response.mode = 1 if self.estop.manual() else 0
-        response.drive_x = self.estop.drive_x1()
-        response.drive_y = self.estop.drive_y1()
+        response.drive_x = -self.estop.drive_x2()
+        response.drive_y = -self.estop.drive_y2()
         # TODO: Implement side strafe using drive x2 and y2 (add those to the .srv as well)
         response.is_connected = bool(self.estop.connected())
         response.is_estopped = bool(self.main_pow.estop() or (not self.estop.connected()) or self.estop.remote_estop())
