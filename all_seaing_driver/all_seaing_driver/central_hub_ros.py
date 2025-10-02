c5e0b4428ce25fa727ff3212476347a5fd8d7b94
asdf1234 <dskprogramming@gmail.com>
2025-10-02 19:42:15 -0400

    Implemented low battery check by adding a variable to GetEstopStatus.srv. Prevents boat from moving when battery is low, unless user types a command to acknowledge this.

--- all_seaing_driver/all_seaing_driver/central_hub_ros.py
+++ all_seaing_driver/all_seaing_driver/central_hub_ros.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
+
+
+"""
+in estpostatus srv, include something about low battery
+check this in rover custom controller line 67
+"""
+
 import rclpy
 from rclpy.node import Node
 from all_seaing_driver.ArcturusEE import BMS, main_power, ESTOP, mech_power
-from all_seaing_interfaces.srv import CommandAdj, CommandServo, GetEstopStatus, CommandFan
+from all_seaing_interfaces.srv import CommandAdj, CommandServo, GetEstopStatus, CommandFan, AcknowledgeLowBattery
 import serial
 import time
 
@@ -14,6 +21,11 @@ class CentralHubROS(Node):
 
         port = self.declare_parameter(
             "port", "/dev/ttyACM0").get_parameter_value().string_value
+    
+        self.low_battery_threshold = self.declare_parameter(
+            "low_battery_threshold", 20.0
+        ).get_parameter_value().double_value
+
         ser = serial.Serial(port, 115200, timeout=1)
         self.main_pow = main_power(ser)
         self.bms = BMS(ser, 0x02)
@@ -43,6 +55,26 @@ class CentralHubROS(Node):
             "command_fan",
             self.cmd_fan_cb,
         )
+        
+        self.ack_low_battery_srv = self.create_service(
+            AcknowledgeLowBattery,
+            "acknowledge_low_battery",
+            self.ack_low_battery_cb
+        )
+
+        self.create_timer(5.0, self.low_battery_check)
+        self.battery_ack = False # true if user acknowledges that the battery is low
+
+    def ack_low_battery_cb(self, request, response):
+        if request.ack:
+            self.battery_ack = True
+            self.get_logger().info("Low battery acknowledged.")
+        else:
+            self.battery_ack = False
+            self.get_logger().info("Low battery UN-acknowledged.")
+        
+        response.success = True
+        return response
 
     def cmd_adj_cb(self, request, response):
         if request.enable:
@@ -67,6 +99,18 @@ class CentralHubROS(Node):
                 return response
         response.success = True
         return response
+    
+    
+    # TRUE if battery is OKAY, FALSE if battery is LOW
+    def battery_check(self):
+        check, volt = self.check_battery_cb()
+        current_voltage = self.bms.stack_voltage()
+        if current_voltage < self.low_battery_threshold and not self.estop.manual():
+            if not self.battery_ack:
+                self.get_logger().warn(f"Warning: battery is low! {volt}V < {self.low_battery_threshold}V")
+                self.get_logger().warn("Please run the command: ros2 service call /acknowledge_low_battery all_seaing_interfaces/srv/AcknowledgeLowBattery \"{acknowledge: true}\" in order to continue")
+                return False
+        return True
 
     def cmd_servo_cb(self, request, response):
         # TODO: Implement when we know how many servos we are going to have/how we are going to use them & they are incorporated in the driver file
@@ -99,6 +143,9 @@ class CentralHubROS(Node):
         # TODO: Implement side strafe using drive x2 and y2 (add those to the .srv as well)
         response.is_connected = bool(self.estop.connected())
         response.is_estopped = bool(self.main_pow.estop())
+
+        # Added a field in GetEstopStatus to check if the battery is okay
+        response.battery_ok = self.battery_check()
         return response
     
     def cmd_fan_cb(self, request, response):
