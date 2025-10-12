96d958f1c606125750caddcdf70a6454a24ec167
Panagiotis Liampas <pliam1105@gmail.com>
2025-10-12 05:40:14 -0400

    minor stuff

--- all_seaing_bringup/launch/sim.launch.py
+++ all_seaing_bringup/launch/sim.launch.py
@@ -465,9 +465,9 @@ def launch_setup(context, *args, **kwargs):
         PythonLaunchDescriptionSource([vrx_gz_prefix, "/launch/competition.launch.py"]),
         launch_arguments={
             # "world": "rb2025/rb2025_task1_task2.sdf",
-            # "world": "follow_path_task.sdf",
+            "world": "follow_path_task.sdf",
             # "world": "speed_course_world.sdf",
-            "world": "scan_dock_deliver_task.sdf",
+            # "world": "scan_dock_deliver_task.sdf",
             "urdf": f"{description_prefix}/urdf/xdrive_wamv/wamv_target.urdf",
             "extra_gz_args": extra_gz_args,
         }.items(),
@@ -516,7 +516,7 @@ def generate_launch_description():
                 "no_gui", default_value="false", choices=["true", "false"]
             ),
             DeclareLaunchArgument(
-                "use_waypoint_client", default_value="true", choices=["true", "false"]
+                "use_waypoint_client", default_value="false", choices=["true", "false"]
             ),
             OpaqueFunction(function=launch_setup),
         ]
