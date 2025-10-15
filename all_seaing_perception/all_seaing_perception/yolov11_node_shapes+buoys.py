2d1773528c6a812818ce17a7f0062eb6a6406446
Sara Koshi <126622391+RoboticLions@users.noreply.github.com>
2025-10-15 22:22:38 +0000

    added node with shape & buoy model, need to test

---
+++ all_seaing_perception/all_seaing_perception/yolov11_node_shapes+buoys.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile
+from rclpy.qos import QoSHistoryPolicy
+from rclpy.qos import QoSDurabilityPolicy
+from rclpy.qos import QoSReliabilityPolicy
+
+from ament_index_python.packages import get_package_share_directory
+from cv_bridge import CvBridge
+
+from ultralytics import YOLO
+from ultralytics.engine.results import Results
+from ultralytics.utils.plotting import Annotator
+
+from all_seaing_interfaces.msg import LabeledBoundingBox2D, LabeledBoundingBox2DArray
+from sensor_msgs.msg import Image
+
+import os
+import torch
+import yaml
+
+
+class Yolov11Node_shapes_buoys(Node):
+
+    def __init__(self):
+        super().__init__("yolov11_node_shapes_buoys")
+
+        perception_prefix = get_package_share_directory("all_seaing_perception")
+        bringup_prefix = get_package_share_directory("all_seaing_bringup")
+
+        # Declare/get ROS parameters
+        # Have both best.pt and last.pt
+        # Want to test both?
+        model_name_buoy = self.declare_parameter(
+            "model", "best").get_parameter_value().string_value
+        model_name_shape = self.declare_parameter(
+            "model", "roboboat_shape_2025").get_parameter_value().string_value
+        self.device = self.declare_parameter(
+            "device", "default").get_parameter_value().string_value
+        self.conf = self.declare_parameter(
+            "conf", 0.6).get_parameter_value().double_value
+        label_config = self.declare_parameter(
+            "label_config", "color_label_mappings").get_parameter_value().string_value
+        self.use_color_names = self.declare_parameter(
+            "use_color_names", True).get_parameter_value().bool_value
+
+
+        if self.device == "default":
+            self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        self.get_logger().info(f'YOLO DEVICE USED: {self.device}')
+
+        label_config = self.get_parameter("label_config").get_parameter_value().string_value
+        yaml_file_path = os.path.join(bringup_prefix, "config","perception", label_config + ".yaml")
+
+        with open(yaml_file_path,"r") as f:
+            self.label_dict = yaml.safe_load(f)
+
+        # Get the model's path
+        engine_path_buoy = os.path.join(perception_prefix, "models", model_name_buoy + ".engine")
+        pt_path_buoy = os.path.join(perception_prefix, "models", model_name_buoy + ".pt")
+        engine_path_shape = os.path.join(perception_prefix, "models", model_name_shape + ".engine")
+        pt_path_shape = os.path.join(perception_prefix, "models", model_name_shape + ".pt")
+
+        # Initialize YOLO model
+        # check if we are using the jetson and if the engine path exists.
+        # Disable tensorRT engine portion??
+        self.cv_bridge = CvBridge()
+        # Loads buoy model
+        if os.path.exists("/etc/nv_tegra_release") and os.path.isfile(engine_path_buoy):
+            self.get_logger().info(f"Loading buoy model from tensorRT engine: {engine_path_buoy}")
+            self.model_buoy = YOLO(engine_path_buoy)
+        elif os.path.isfile(pt_path_buoy):
+            self.get_logger().info(f"Loading buoy model from pt model: {pt_path_buoy}")
+            self.model_buoy = YOLO(pt_path_buoy)
+        else:
+            self.get_logger().error(f"Both buoy model paths do not exist :( TensorRT: {engine_path_buoy} and pt: {pt_path_buoy}")
+
+        # Loads shape model
+        if os.path.exists("/etc/nv_tegra_release") and os.path.isfile(engine_path_shape):
+            self.get_logger().info(f"Loading shape model from tensorRT engine: {engine_path_shape}")
+            self.model_shape = YOLO(engine_path_shape)
+        elif os.path.isfile(pt_path_shape):
+            self.get_logger().info(f"Loading shape model from pt model: {pt_path_shape}")
+            self.model_shape = YOLO(pt_path_shape)
+        else:
+            self.get_logger().error(f"Both shape model paths do not exist :( TensorRT: {engine_path_shape} and pt: {pt_path_shape}")
+
+        # Setup QoS profile
+        image_qos_profile = QoSProfile(
+            reliability=QoSReliabilityPolicy.BEST_EFFORT,
+            history=QoSHistoryPolicy.KEEP_LAST,
+            durability=QoSDurabilityPolicy.VOLATILE,
+            depth=1
+        )
+
+        # Publisher and Subscriber
+        self._pub = self.create_publisher(LabeledBoundingBox2DArray, "bounding_boxes", 10)
+        self._image_pub = self.create_publisher(Image, "annotated_image", 10)
+        # Put image from subscription through HSV filtering to help accuracy??
+        # Still have the one from HSV filtering before
+        self._sub = self.create_subscription(Image, "image", self.image_cb, image_qos_profile)
+
+    def image_cb(self, msg: Image) -> None:
+
+        # Convert image to cv_image
+        cv_image = self.cv_bridge.imgmsg_to_cv2(msg, "bgr8")
+        annotator = Annotator(cv_image, 2, 2, "Arial.ttf", False)
+
+        # Append Buoy Results
+        pred_results_buoy = self.model_buoy.predict(
+            source=cv_image,
+            verbose=False,
+            stream=False,
+            conf=self.conf,
+            device=self.device
+        )
+        results_buoy: Results = pred_results_buoy[0].cpu()
+
+        labeled_bounding_box_msgs = LabeledBoundingBox2DArray()
+        labeled_bounding_box_msgs.header = msg.header
+
+        for box_data in results_buoy.boxes:
+
+            box_msg = LabeledBoundingBox2D()
+
+            if results_buoy.boxes:
+
+                box_msg.probability = float(box_data.conf)
+                center_x, center_y, width, height = box_data.xywh[0]
+                box_msg.min_x = int(center_x - width / 2)
+                box_msg.max_x = int(center_x + width / 2)
+                box_msg.min_y = int(center_y - height / 2)
+                box_msg.max_y = int(center_y + height / 2)
+                label_name = self.model_buoy.names[int(box_data.cls)]
+
+                labeled_bounding_box_msgs.boxes.append(box_msg)
+
+                class_name = f"{label_name}_{str(int(box_data.cls))}"
+                # Only adds the objects detected that are buoys
+                if "buoy" in class_name:
+                    class_name_list = class_name.split("_")
+                    color_name = class_name_list[0] # assumes labels follow "color_object" format
+                    color = ()
+                    text_color = (0,0,0)
+                    if color_name == "red":
+                        color = (0, 0, 255)
+                    elif color_name == "green":
+                        color = (0,255,0)
+                    elif color_name == "blue":
+                        color = (255, 0, 0)
+                    elif color_name == "yellow":
+                        color = (0,230,230)
+                    elif color_name == "black":
+                        color = (0,0,0)
+                    elif color_name == "white":
+                        color = (255,255,255)
+                    else:
+                        color = (128,128,128)
+
+                    color_label = -1
+                    if color_name in self.label_dict:
+                        color_label = self.label_dict[color_name]
+                        # if color_name in ["black", "blue", "red", "green"]:
+                        #     object_name = class_name_list[1]
+                        #     if "buoy" not in object_name:
+                        #         color_label = -1
+
+                        annotator.box_label((box_msg.min_x, box_msg.min_y, box_msg.max_x, box_msg.max_y), class_name, color, text_color)
+                        self.get_logger().debug(f"Detected: {class_name} Msg Label is {box_msg.label}")
+                    else:
+                        box_msg.label = -1 # for misc things
+                        annotator.box_label((box_msg.min_x, box_msg.min_y, box_msg.max_x, box_msg.max_y), class_name, color, text_color)
+                        self.get_logger().debug(f"Detected: {class_name} Item not in label_dict")
+
+                    if self.use_color_names:
+                        box_msg.label = color_label
+                    else:
+                        box_msg.label = int(box_data.cls)
+
+        # Append shape results
+        # Assumed would use same code for annotating as the buoy model
+        # Might have problem with two bounding boxes for one buoy/shape?
+        pred_results_shape = self.model_shape.predict(
+            source=cv_image,
+            verbose=False,
+            stream=False,
+            conf=self.conf,
+            device=self.device
+        )
+        results_shape: Results = pred_results_shape[0].cpu()
+
+        for box_data in results_shape.boxes:
+
+            box_msg = LabeledBoundingBox2D()
+
+            if results_shape.boxes:
+
+                box_msg.probability = float(box_data.conf)
+                center_x, center_y, width, height = box_data.xywh[0]
+                box_msg.min_x = int(center_x - width / 2)
+                box_msg.max_x = int(center_x + width / 2)
+                box_msg.min_y = int(center_y - height / 2)
+                box_msg.max_y = int(center_y + height / 2)
+                # Uses names from only one model name set
+                label_name = self.model_buoy.names[int(box_data.cls)]
+
+                labeled_bounding_box_msgs.boxes.append(box_msg)
+
+                # Adds all bouding boxes detected by model
+                class_name = f"{label_name}_{str(int(box_data.cls))}"
+                class_name_list = class_name.split("_")
+                color_name = class_name_list[0] # assumes labels follow "color_object" format
+                color = ()
+                text_color = (0,0,0)
+                if color_name == "red":
+                    color = (0, 0, 255)
+                elif color_name == "green":
+                    color = (0,255,0)
+                elif color_name == "blue":
+                    color = (255, 0, 0)
+                elif color_name == "yellow":
+                    color = (0,230,230)
+                elif color_name == "black":
+                    color = (0,0,0)
+                elif color_name == "white":
+                    color = (255,255,255)
+                else:
+                    color = (128,128,128)
+
+                color_label = -1
+                if color_name in self.label_dict:
+                    color_label = self.label_dict[color_name]
+                    # if color_name in ["black", "blue", "red", "green"]:
+                    #     object_name = class_name_list[1]
+                    #     if "buoy" not in object_name:
+                    #         color_label = -1
+
+                    annotator.box_label((box_msg.min_x, box_msg.min_y, box_msg.max_x, box_msg.max_y), class_name, color, text_color)
+                    self.get_logger().debug(f"Detected: {class_name} Msg Label is {box_msg.label}")
+                else:
+                    box_msg.label = -1 # for misc things
+                    annotator.box_label((box_msg.min_x, box_msg.min_y, box_msg.max_x, box_msg.max_y), class_name, color, text_color)
+                    self.get_logger().debug(f"Detected: {class_name} Item not in label_dict")
+
+                if self.use_color_names:
+                    box_msg.label = color_label
+                else:
+                    box_msg.label = int(box_data.cls)
+
+
+        # Publish detections for both buoy and shape on one image
+        self._pub.publish(labeled_bounding_box_msgs)
+
+        # Convert annotated image back to ROS Image message
+        annotated_frame = annotator.result()
+        annotated_image_msg = self.cv_bridge.cv2_to_imgmsg(annotated_frame, encoding="bgr8")
+        self._image_pub.publish(annotated_image_msg)  # Publish annotated image
+
+
+def main():
+    rclpy.init()
+    node = Yolov11Node_shapes_buoys()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
