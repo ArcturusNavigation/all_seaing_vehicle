928687e34f1380554ee7d8a44d46f7010411df8d
Panagiotis Liampas <pliam1105@gmail.com>
2026-01-28 19:23:07 -0500

    new shape + buoy model

--- all_seaing_perception/all_seaing_perception/yolov8_node.py
+++ all_seaing_perception/all_seaing_perception/yolov8_node.py
@@ -131,6 +131,7 @@ class Yolov8Node(Node):
                     color = (0,230,230)
                 elif color_name == "black":
                     color = (0,0,0)
+                    text_color = (255,255,255)
                 elif color_name == "white":
                     color = (255,255,255)
                 else: 
