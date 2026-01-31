4dbb19cf2d2c984cf4b1e6cadf575e49e189581e
Panagiotis Liampas <pliam1105@gmail.com>
2026-01-31 13:10:23 -0500

    docking numbers + model incorporated

--- all_seaing_autonomy/all_seaing_autonomy/roboboat/docking.py
+++ all_seaing_autonomy/all_seaing_autonomy/roboboat/docking.py
@@ -200,8 +200,8 @@ class Docking(TaskServerBase):
             # TODO replace w/ numbers for the dock banner labels, as in the roboboat course
             # self.dock_labels = [self.label_mappings[name] for name in ["blue_circle", "blue_cross", "blue_triangle", "green_circle", "green_cross", "green_square", "green_triangle", "red_circle", "red_cross", "red_triangle", "red_square"]]
             # self.boat_labels = [self.label_mappings[name] for name in ["black_cross", "black_triangle"]]
-            self.dock_labels = [self.label_mappings[name] for name in ["black_cross", "black_triangle"]]
-            self.boat_labels = []
+            self.dock_labels = [self.label_mappings[name] for name in ["number_1", "number_2", "number_3"]]
+            self.boat_labels = [self.label_mappings[name] for name in ["black_cross", "black_triangle"]]
         
         self.inv_label_mappings = {}
         for key, value in self.label_mappings.items():
