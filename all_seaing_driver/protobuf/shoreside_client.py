2b2dd44f252bee1d16b99fdb47c382cd00df62d4
Brendon Jiang <brendon.jiang1234567890@gmail.com>
2026-02-12 19:25:05 -0500

    better file names

--- all_seaing_driver/protobuf/shoreside_client.py
+++ all_seaing_driver/protobuf/shoreside_client.py
@@ -34,7 +34,7 @@ def read_frame(ser):
 
 # Setup serial connection
 # serial_port = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
-serial_port = serial.Serial('/dev/tty.usbmodem2080345752471', 115200, timeout=1)
+serial_port = serial.Serial('/dev/tty.usbmodem2089346352471', 115200, timeout=1)
 time.sleep(2)  # Allow serial port to stabilize
 
 while True:
